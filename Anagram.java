2e46bf42efd031b54daf847ed9827f780a9880ae
Elliot Parker <elliotccp@protonmail.com>
2020-08-06 17:14:27 +1000

    Add enum

--- Anagram.java
+++ Anagram.java
@@ -1,7 +1,11 @@
 public class Anagram {
     private String str1;
     private String str2;
-    private Boolean areAnagram;
+    private AreAnagram areAnagram;
+
+    private enum AreAnagram {
+        ARE, ARENT
+    };
 
     public Anagram(String str1, String str2) {
         this.str1 = str1;
@@ -9,10 +13,14 @@ public class Anagram {
     }
 
     public void setResult(Boolean result) {
-        this.areAnagram = result;
+        if (result) {
+            this.areAnagram = AreAnagram.ARE;
+        } else {
+            this.areAnagram = AreAnagram.ARENT;
+        }
     }
 
     public String toString() {
-        return this.str1 + " and " + this.str2 + ": " + this.areAnagram;
+        return this.str1 + " and " + this.str2 + " " + this.areAnagram + " anagrams";
     }
 }
